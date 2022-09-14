67fecfba9d76e3352fd7cc69782b2b91bc680d54
Lconforme <Lconforme06@yahoo.com>
2022-09-14 21:49:37 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function nameGenerator() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".org", ".gov", ".net"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < domain.length; d++)
+          console.log(`${pronoun[p]}${adj[a]}${noun[n]}${domain[d]}`);
+      }
+    }
+  }
+}
+console.log(nameGenerator());
